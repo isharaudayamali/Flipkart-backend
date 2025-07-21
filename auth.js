2e4977ed2ab8d2f85a3d3500fb7b1a33c9bb35c5
Ishara Udayamali <isharaudayamali220@gmail.com>
2025-07-21 17:02:53 +0000

    login and Signup backend

--- auth.js
+++ auth.js
@@ -1,26 +1,56 @@
-const express = require ("express");
-const mongoose = require ("mongoose");
-const jwt = require ("jsonwebtoken");
-const bcript = require("bcript");
-const router = express.router();
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcript");
+const router = express.Router();
 
-const User =mongoose.model('User',new mongoose,Schema
-( 
-   { email:String,
-    password:String}
-));
+const User = mongoose.model(
+  "User",
+  new mongoose(),
+  Schema({ email: String, password: String })
+);
 
 //signup router
-router.post("/auth/signup",async(req,res)=>{
-const {email.password}=> req.body;
-const existingUser=await User.findOne({email});
-if(existingUser){
-    return res.status(400), json({error:"already exist"});
-}
-const hashedPassword =awit bcript.hash([password]);
-const user=new User ({email,password:hashedPassword});
-await user.save();
-const token=jwt.sign({userId:user._id},'secrest',{expireIn:'1h'});
-res.status(200).json({token});
+router.post("/auth/signup", async (req, res) => {
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400), json({ error: "already exist" });
+  }
+  const hashedPassword = await bcript.hash([password]);
+  const user = new User({ email, password: hashedPassword });
+  await user.save();
+  const token = jwt.sign({ userId: user._id }, "secret", { expireIn: "1h" });
+  res.status(200).json({ token });
+});
 
-})
+//login router
+
+router.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ userId: user._id }, "secret", { expiresIn: "1h" });
+  } else {
+    res.status(400).json({ error: "Invalid credential" });
+  }
+});
+
+//jwt middleware
+function authenticateJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split("")[1];
+    jwt.verify(token, "secret", (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+}
+
+module.exports = { router, authenticateJWT };
