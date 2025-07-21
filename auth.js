c0c31e48cc2a323e06283346c343bf0b9b63377a
CodeSandbox <support@codesandbox.io>
2025-07-21 16:00:12 +0000

    Sandbox snapshot commit

--- auth.js
+++ auth.js
@@ -1,3 +1,17 @@
 const express = require ("express");
 const mongoose = require ("mongoose");
-const jwt = require ("c");
+const jwt = require ("jsonwebtoken");
+const bcript = require("bcript");
+const router = express.router();
+
+const User =mongoose.model('User',new mongoose,Schema
+( 
+   { email:String,
+    password:String}
+));
+
+//signup router
+router.post("/auth/signup",async(req,res)=>
+const {email.password}=> req.body;
+const existing
+)
